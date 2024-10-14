8d3ed1687928469bab176eb63fe11805bb7a97ef
Md. Zubayer Ahmad Shibly <159265436+iamshibly@users.noreply.github.com>
2024-10-14 19:06:17 +0600

    Add files via upload

---
+++ 6. method inside main method/6. method inside main method/src/Basic.java
@@ -0,0 +1,10 @@
+public class Basic {
+    public static void main(String [] args){
+        int save = abc(6,4);
+        System.out.println(save);
+
+    }
+    static int abc(int a, int b){
+        return a+b;
+    }
+}
